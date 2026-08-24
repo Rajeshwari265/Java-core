231d9a2fe2e33b418e0f563b60b27b9c1d7e39b1
RajeshwariSelvaraj <rajeshwariselvaraj929@gmail.com>
2026-08-24 11:34:20 +0530

    Add files via upload

---
+++ ValidPalindrome.java
@@ -0,0 +1,36 @@
+package Jvacore;
+public class ValidPalindrome {
+    public static boolean isPalindrome(String s) {
+        if (s == null) return false;
+        
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+           
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            
+            
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String test = "A man, a plan, a canal: Panama";
+        System.out.println("Is palindrome: " + isPalindrome(test));
+        // Output: Is palindrome: true
+    }
+}
+
