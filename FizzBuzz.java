d4d57b246403da63998855aa0a23f42524c8ae27
RajeshwariSelvaraj <rajeshwariselvaraj929@gmail.com>
2026-08-24 11:23:19 +0530

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,22 @@
+package Jvacore;
+
+public class FizzBuzz {
+    public static void printFizzBuzz(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        printFizzBuzz(15);
+         
+    }
+}
