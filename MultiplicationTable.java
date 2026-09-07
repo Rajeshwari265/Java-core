a9e7ba67bf977fb7e90de57b2daa012f95c5be81
RajeshwariSelvaraj <rajeshwariselvaraj929@gmail.com>
2026-09-07 20:10:53 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,9 @@
+package Jvacore;
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int tableOf = 5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(tableOf + " x " + i + " = " + (tableOf * i));
+        }
+    }
+}
