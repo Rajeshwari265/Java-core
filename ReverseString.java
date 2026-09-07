e0f6eac4f750dea2c59cc176a1585a78ad364b36
RajeshwariSelvaraj <rajeshwariselvaraj929@gmail.com>
2026-09-07 20:16:39 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,15 @@
+package Jvacore;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String original = "Java Programming";
+        String reversed = "";
+        
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
+        }
+        
+        System.out.println("Reversed: " + reversed);
+    }
+}
+   
